b5f05a9489d50af7a8e04e9868e982d04716ce88
layden17 <120340868+layden17@users.noreply.github.com>
2022-12-15 20:45:36 +0100

    Add files via upload

---
+++ Test/ProjetDebat/debat/ExceptionMenuTest.java
@@ -0,0 +1,33 @@
+package TestUnitaires;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import debat.ExceptionMenu;
+
+
+class ExceptionMenuTest {
+
+	private ExceptionMenu expM;
+	
+	@BeforeAll
+	void beforeAll(){
+		expM= new ExceptionMenu("Erreur, dans votre classe Menu");
+	}
+	
+	@Test
+	void testExceptionMenu() {
+		assertEquals(false,expM.equals(null));
+		
+	}
+
+	@Test
+	void testGetMessage() {
+		assertEquals("Erreur, dans votre classe Menu",expM.getMessage());
+	}
+
+
+}
