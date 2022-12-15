e740b3f24ab3edb5b78e592df7f52144199f1953
layden17 <120340868+layden17@users.noreply.github.com>
2022-12-15 18:37:55 +0100

    Add files via upload

---
+++ Test/ProjetDebat/debat/GrapheTest.java
@@ -0,0 +1,88 @@
+package TestUnitaires;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import graphe.Argument;
+import graphe.Graphe;
+
+class GrapheTest {
+	
+	private Argument arg;
+	private Argument arg2;
+	private Argument arg3;
+	private Argument arg4;
+	private Argument arg5;
+	ArrayList<Argument>listArguments;
+	private Graphe gphe;
+	private Graphe gphe2;
+	
+	
+	@BeforeEach
+	void beforeAll(){
+		arg = new Argument("A");
+		arg2 = new Argument("B");
+		arg3 = new Argument("C");
+		arg4 = new Argument("D");
+	
+		listArguments = new ArrayList<Argument>();
+		
+		listArguments.add(arg);
+		listArguments.add(arg2);
+		listArguments.add(arg3);
+		listArguments.add(arg4);
+		
+		//graphe faite avec une liste d'arguments en paramètre
+		gphe = new Graphe(listArguments);
+		//graphe faite sans paramètre
+		gphe2 = new Graphe();
+		
+		gphe2.ajouterNoeud(arg);
+		gphe2.ajouterArc(arg, arg2);
+		gphe2.ajouterArc(arg, arg3);
+		
+	}
+	@Test
+	void testGrapheListOfArgument() {
+		assertEquals(true,gphe.getGraphe().containsKey(arg));
+		assertEquals(true,gphe.getGraphe().containsKey(arg2));
+		assertEquals(true,gphe.getGraphe().containsKey(arg3));
+		assertEquals(true,gphe.getGraphe().containsKey(arg4));
+		assertEquals(false,gphe.getGraphe().containsKey(arg5));
+	}
+
+	@Test
+	void testAjouterNoeud() {
+		assertEquals(true,gphe2.getGraphe().containsKey(arg));
+		assertEquals(false,gphe2.getGraphe().containsKey(arg2));
+	}
+
+	@Test
+	void testAjouterArc() {
+		assertEquals(true,gphe2.getGraphe().get(arg).contains(arg2));
+		assertEquals(true,gphe2.getGraphe().get(arg).contains(arg3));
+		assertEquals(false,gphe2.getGraphe().get(arg).contains(arg4));
+	}
+
+	@Test
+	void testTrouverArgNom_1() {
+		assertEquals(arg,gphe.trouverArgNom("A"));
+	}
+	@Test
+	void testTrouverArgNom_2() {
+		assertEquals(null,gphe.trouverArgNom("X"));
+	}
+
+
+	@Test
+	void testToString() {
+		assertEquals("\n"+ "L'Argument A, contredit les arguments suivants : \n"+"\n\t Argument B\n"+"\n\t Argument C\n",gphe2.toString());
+	}
+
+
+}
