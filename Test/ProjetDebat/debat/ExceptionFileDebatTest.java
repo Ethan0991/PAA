602a6559b3ce080bde96c54275ea07e60631ed07
layden17 <120340868+layden17@users.noreply.github.com>
2022-12-16 11:27:50 +0100

    Add files via upload

---
+++ Test/ProjetDebat/debat/ExceptionFileDebatTest.java
@@ -0,0 +1,35 @@
+package TestUnitaires;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import file.ExceptionFileDebat;
+
+/**
+ * Classe permettant de tester le fichier ExceptionFileDebat.java
+ *
+ * @author Ethan & Lathan
+ */
+class ExceptionFileDebatTest {
+	
+	private ExceptionFileDebat expFD;
+
+	@BeforeAll
+	void beforeAll(){
+		expFD= new ExceptionFileDebat("Erreur, dans votre classe Fichier Débat");
+	}
+	
+	@Test
+	void testExceptionFileDebat() {
+		assertEquals(false,expFD.equals(null));
+	}
+
+	@Test
+	void testGetMessage() {
+		assertEquals("Erreur, dans votre classe Fichier Débat",expFD.getMessage());
+	}
+
+}
---
+++ Test/ProjetDebat/debat/ExceptionMenuTest.java
@@ -0,0 +1,34 @@
+package ProjetDebat.debat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Classe permettant de tester le fichier ExceptionMenu.java
+ *
+ * @author Ethan & Lathan
+ */
+class ExceptionMenuTest {
+
+	private ExceptionMenu expM;
+	
+	@BeforeAll
+	void beforeAll(){
+		expM= new ExceptionMenu("Erreur, dans votre classe Menu");
+	}
+	
+	@Test
+	void testExceptionMenu() {
+		assertEquals(false,expM.equals(null));
+		
+	}
+
+	@Test
+	void testGetMessage() {
+		assertEquals("Erreur, dans votre classe Menu",expM.getMessage());
+	}
+
+
+}
